fe9e8f2d468264535c8a28a13c0c2f588e7a8a00
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2025-08-22 22:34:57 +0530

    feat: implement routing with react-router-dom and update App structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import ChatBox from './components/ChatBox'
+import Credits from './pages/Credits'
+import Community from './pages/Communtiy'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='dark:bg-gradient-to-b dark:from-[#242124] dark:to-[#000000] dark:text-white'>
+      <div className='flex h-screen w-screen'>
+        <Sidebar/>
+      <Routes>
+         <Route path='/' element={<ChatBox/>} />
+         <Route path='/credits' element={<Credits/>} />
+         <Route path='/community' element={<Community/>} />
+      </Routes>
+      </div>
+    </div>
   )
 }
 
